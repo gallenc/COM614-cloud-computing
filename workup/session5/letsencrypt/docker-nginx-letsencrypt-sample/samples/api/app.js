ca73c6e8ca66c76479292363d518721d6ebade27
gallenc <cg02r@zepler.net>
2026-03-31 17:18:05 +0100

    fixing web

--- workup/session5/letsencrypt/docker-nginx-letsencrypt-sample/samples/api/app.js
+++ workup/session5/letsencrypt/docker-nginx-letsencrypt-sample/samples/api/app.js
@@ -1,13 +1,44 @@
-var express = require('express');
-var cors = require('cors');
+//var express = require('express');
+//var cors = require('cors');
+//
+//var app = express();
+//app.use(cors());
+//
+//app.get('/api/hello', function (req, res) {
+//  res.send('Hello World!');
+//});
+//
+//app.listen(3001, function () {
+//  console.log('Listening on port 3000.');
+//});
 
+
+var express = require('express');
 var app = express();
 app.use(cors());
 
-app.get('/api/hello', function (req, res) {
-  res.send('Hello World!');
+var router = express.Router();
+
+// simple logger for this router's requests
+// all requests to this router will first hit this middleware
+router.use(function(req, res, next) {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  next();
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000.');
+// this will only be invoked if the path ends in /bar
+router.use('/api', function(req, res, next) {
+  res.send('Hello World!');
+  next();
+});
+
+// always invoked
+//router.use(function(req, res, next) {
+//  res.send('Hello World');
+//});
+
+app.use('/', router);
+
+app.listen(3001, function () {
+   console.log('Listening on port 3000.');
 });
